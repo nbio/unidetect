ca1316827c6096cb9c983500450dabf3fda22ad5
Randy Reddig <randy@domainr.com>
2021-03-29 12:45:58 -0700

    Remove dependency on github.com/nbio/st

--- go.mod
+++ go.mod
@@ -1,5 +1,3 @@
 module github.com/nbio/unidetect
 
 go 1.16
-
-require github.com/nbio/st v0.0.0-20140626010706-e9e8d9816f32
--- unidetect_test.go
+++ unidetect_test.go
@@ -1,37 +1,43 @@
 package unidetect
 
 import (
-	"regexp"
+	"reflect"
+	"strings"
 	"testing"
-
-	"github.com/nbio/st"
 )
 
+var w = strings.Fields
+
 func TestDetectScripts(t *testing.T) {
-	st.Expect(t, DetectScripts("abc"), w(`Latin`))
-	st.Expect(t, DetectScripts("北京"), w(`Han`))
-	st.Expect(t, DetectScripts("北京beijing"), w(`Han Latin`))
-	st.Expect(t, DetectScripts("⠋⠗"), w(`Braille`))
-	st.Expect(t, DetectScripts("straße"), w(`Latin`))
-	st.Expect(t, DetectScripts("みんな"), w(`Hiragana`))
-	st.Expect(t, DetectScripts("カタカナ"), w(`Katakana`))
-	st.Expect(t, DetectScripts("москва"), w(`Cyrillic`))
-	st.Expect(t, DetectScripts("קוֹם"), w(`Hebrew`))
-	st.Expect(t, DetectScripts("ابوظبي"), w(`Arabic`))
-	st.Expect(t, DetectScripts("कॉम"), w(`Devanagari`))
-	st.Expect(t, DetectScripts("닷넷"), w(`Hangul`))
-	st.Expect(t, DetectScripts("გე"), w(`Georgian`))
-	st.Expect(t, DetectScripts("இ"), w(`Tamil`))
-	st.Expect(t, DetectScripts("భారత్"), w(`Telugu`))
-	st.Expect(t, DetectScripts("ලංකා"), w(`Sinhala`))
-}
-
-func w(s string) []string {
-	out := ws.Split(s, -1)
-	if len(out) == 1 && out[0] == "" {
-		return nil
+	tests := []struct {
+		s    string
+		want []string
+	}{
+		{"abc", w(`Latin`)},
+		{"北京", w(`Han`)},
+		{"北京beijing", w(`Han Latin`)},
+		{"⠋⠗", w(`Braille`)},
+		{"straße", w(`Latin`)},
+		{"みんな", w(`Hiragana`)},
+		{"カタカナ", w(`Katakana`)},
+		{"москва", w(`Cyrillic`)},
+		{"קוֹם", w(`Hebrew`)},
+		{"ابوظبي", w(`Arabic`)},
+		{"कॉम", w(`Devanagari`)},
+		{"닷넷", w(`Hangul`)},
+		{"გე", w(`Georgian`)},
+		{"இ", w(`Tamil`)},
+		{"భారత్", w(`Telugu`)},
+		{"ලංකා", w(`Sinhala`)},
 	}
-	return out
-}
 
-var ws = regexp.MustCompile(`\s+`)
+	for _, tt := range tests {
+		t.Run(tt.s, func(t *testing.T) {
+			got := DetectScripts(tt.s)
+			if !reflect.DeepEqual(tt.want, got) {
+				t.Errorf("DetectScripts(%v) = %v, want %v", tt.s, got, tt.want)
+			}
+		})
+	}
+
+}
